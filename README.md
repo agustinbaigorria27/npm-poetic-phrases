9a771d4737574b62a09edb1605d2af37c954e4b9
[Agustin Baigorria] <[agustinbaigorria27@gmail.com]>
2021-08-20 23:27:47 -0300

    Paquete finalizado

---
+++ README.md
@@ -0,0 +1,21 @@
+# random-poems
+
+This is a package that provides famous quotes randomly to improve our day.
+
+## Install 
+
+```npm
+npm install -g random-msg
+```
+
+# Usage 
+
+```bash
+ramdom-msg
+```
+
+# Contributing 
+If someone wants to add or improve something, I invite you to collaborate directly in this repository: [random-poems]
+
+# License
+random-msg is realeased under the [MIT License](https://opensource.org/licenses/MIT).
---
+++ src/index.js
@@ -0,0 +1,17 @@
+const poems = [
+  "La poesía no quiere adeptos, quiere amantes. 'Federico Garcia Loeca'",
+  "Es propio de aquellos con mentes estrechas embestir contra todo aquello que no les cabe en la cabeza. 'Antonio Machado'",
+  "El libro es fuerza, es valor, es poder, es alimento, antorcha del pensamiento y manantial del amor. 'Rúben Dario",
+  "Requiere coraje crecer y convertirte en lo que realmente eres. 'E.E. Cummings'",
+  "No hay hombre más infeliz que aquel para quien la indecisión se ha hecho costumbre. 'Henrich Heine'",
+  "Esclavo es aquel que espera por alguien que venga y lo libere. 'Ezra Pound'",
+  "Cuando nuestras ideas chocan con la realidad, lo que tiene que ser revisado son las ideas. 'Jorge Luis Borges'",
+  "¿Hola? Soy la mamá Universe. Sí, los niños están jugando a espadas. Perdón, jugando con espadas. Están sangrando. ¡Oh, no! Están muertos. No vuelva a llamar. Lo siento, entré en pánico. 'Garnet'"
+];
+
+const randomPoems = () => {
+  const poem = poems[Math.floor(Math.random() * poems.length)];
+  console.log(poem);
+};
+
+module.exports = { randomPoems };
